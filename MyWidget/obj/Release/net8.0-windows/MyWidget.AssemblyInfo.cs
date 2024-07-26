7d2c5aa332ee0f019576927b7a3e4d5f429b558b
emreaykin <eaykin61@gmail.com>
2024-07-26 13:00:40 +0300

    Icon path updated

--- MyWidget/obj/Release/net8.0-windows/MyWidget.AssemblyInfo.cs
+++ MyWidget/obj/Release/net8.0-windows/MyWidget.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWidget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844d585d9bc3123a56f39953c9e3a165ac43d67c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWidget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWidget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
